1cfa5d9245dcea33c0f2e4b3125bf41ed2a9db51
Mario Schillermann <mario.schillermann@gmail.com>
2026-03-03 09:59:05 +0100

    init library

---
+++ src/main/java/de/schillermann/jpages/App.java
@@ -0,0 +1,7 @@
+package de.schillermann.jpages;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, Gradle!");
+    }
+}
---
+++ src/test/java/de/schillermann/jpages/AppTest.java
@@ -0,0 +1,12 @@
+package de.schillermann.jpages;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AppTest {
+    @Test
+    void appHasMain() {
+        assertTrue(true);
+    }
+}
